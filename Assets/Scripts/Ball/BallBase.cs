9744d2d343a20b0f557e844639cc746b1fdc005e
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-05-05 23:47:11 -0300

    Random Ball X Y
    Criando Movimento randomico da bola

--- Assets/Scripts/Ball/BallBase.cs
+++ Assets/Scripts/Ball/BallBase.cs
@@ -11,6 +11,12 @@ public class BallBase : MonoBehaviour
     [Header ("Variables")]
     public Vector3 speed = new Vector3(1,1);
 
+
+    //randomizar o movimento da bola em X e Y - criando uma nova variavel de Vector em x e y
+    [Header("Randomization")]
+    public Vector2 randSpeedY = new Vector2(1,3);
+    public Vector2 randSpeedX = new Vector2(1,3);
+
     // Update is called once per frame
     void Update()
     {
@@ -22,14 +28,31 @@ public class BallBase : MonoBehaviour
         // assim quando colidir em y (bordas) retorna em valor negativo
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player"){
-            speed.x *= -1;
+            OnPlayerCollision(); 
+            // troca pelo método ali em baixo 
+            /*speed.x *= -1;*/
             Debug.Log("OncollisionPlayer");
         }
         else if(collision.gameObject.tag == "Border"){
             speed.y *= -1;
             Debug.Log("OncollisionWall");
-        } 
+        }
     }
     // esta entrando em colisão
     // então pq não funciona?? - simples - bordas sem rigdbody e uma borda tinha 2 box colider 
+
+    //criando um método para colisão com player para assim deixar ela random
+    private void OnPlayerCollision(){
+        speed.x *= -1;
+
+        float rand = Random.Range(randSpeedX.x,randSpeedX.y);
+
+        // se velocidade for neagtiva - o número rand tb será - fazendo a bola mudar de direção - ao colidir com o player muda de direção
+        if(speed.x < 0)
+            rand = -rand;
+        speed.x = rand;
+
+        rand = Random.Range(randSpeedY.x, randSpeedY.y);
+        speed.y = rand;
+    }
 }
